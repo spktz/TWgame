0ac2b236364b49689c85988d69baa31c742b9230
Roman Mydlovets <ipz243_mrs@student.ztu.edu.ua>
2026-02-23 18:13:02 +0200

    Fix issue #3

--- TankWarsGame/BulletController.cs
+++ TankWarsGame/BulletController.cs
@@ -97,120 +97,151 @@ namespace TankWarsGame
 
                 bullet.Update();
 
-                double bullX = bullet.X;
-                double bullY = bullet.Y;
-                double bullwidth = bullet.Img.Width;
-                double bullhght = bullet.Img.Height;
-
-                if (bullet.X < 0 || bullet.Y < 0 //перевірка виходу за межі канвм
-                 || bullet.X > _canvas.ActualWidth
-                 || bullet.Y > _canvas.ActualHeight)
+                if (IsBulletOutOfBounds(bullet))
                 {
                     bullet.Deactivate();
                     continue;
                 }
 
-                bool hitSmth = false;
+                if (CheckWallCollision(bullet))
+                    continue;
 
-                foreach (var wall in _walls) //перевірка на зіткнення стін
+                if (CheckPlayerCollision(bullet))
+                    continue;
+
+                CheckEnemyCollision(bullet);
+            }
+        }
+
+        private bool IsBulletOutOfBounds(Bullet bullet)
+        {
+            return bullet.X < 0 || bullet.Y < 0
+                || bullet.X > _canvas.ActualWidth
+                || bullet.Y > _canvas.ActualHeight;
+        }
+
+        private bool CheckWallCollision(Bullet bullet)
+        {
+            double bullX = bullet.X;
+            double bullY = bullet.Y;
+            double bullW = bullet.Img.Width;
+            double bullH = bullet.Img.Height;
+
+            foreach (var wall in _walls)
+            {
+                if (wall.IsDestroyed)
+                    continue;
+
+                double wx = Canvas.GetLeft(wall.Img);
+                double wy = Canvas.GetTop(wall.Img);
+                double ww = wall.Img.Width;
+                double wh = wall.Img.Height;
+
+                bool isHit = bullX < wx + ww && bullX + bullW > wx
+                          && bullY < wy + wh && bullY + bullH > wy;
+
+                if (!isHit)
+                    continue;
+
+                bullet.Deactivate();
+
+                double centerX = wx + ww / 2;
+                double centerY = wy + wh / 2;
+                new Explosion(_canvas, centerX, centerY);
+
+                if (wall.Type == TypeWall.Brick)
+                    wall.TakeHit();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckPlayerCollision(Bullet bullet)
+        {
+            if (bullet.OwnerEnemy == null)
+                return false;
+
+            double bullX = bullet.X;
+            double bullY = bullet.Y;
+            double bullW = bullet.Img.Width;
+            double bullH = bullet.Img.Height;
+
+            double px = Canvas.GetLeft(_playerImg);
+            double py = Canvas.GetTop(_playerImg);
+            double pW = _playerImg.Width;
+            double pH = _playerImg.Height;
+
+            bool hitPlayer = bullX < px + pW
+                          && bullX + bullW > px
+                          && bullY < py + pH
+                          && bullY + bullH > py;
+
+            if (!hitPlayer)
+                return false;
+
+            bullet.Deactivate();
+
+            double centerX = px + pW / 2;
+            double centerY = py + pH / 2;
+            new Explosion(_canvas, centerX, centerY);
+
+            _canvas.Children.Remove(bullet.Img);
+            _bullets.Remove(bullet);
+
+            _playerModel.TakeDamage();
+            PlayerHit?.Invoke();
+
+            return true;
+        }
+
+        private void CheckEnemyCollision(Bullet bullet)
+        {
+            if (bullet.OwnerEnemy != null)
+                return;
+
+            double bullX = bullet.X;
+            double bullY = bullet.Y;
+            double bullW = bullet.Img.Width;
+            double bullH = bullet.Img.Height;
+
+            foreach (var enemy in _enemies)
+            {
+                if (enemy.Img.Visibility != Visibility.Visible)
+                    continue;
+
+                double ex = Canvas.GetLeft(enemy.Img);
+                double ey = Canvas.GetTop(enemy.Img);
+                double eW = enemy.Img.Width;
+                double eH = enemy.Img.Height;
+
+                bool hitEnemy = bullX < ex + eW
+                             && bullX + bullW > ex
+                             && bullY < ey + eH
+                             && bullY + bullH > ey;
+
+                if (!hitEnemy)
+                    continue;
+
+                bullet.Deactivate();
+
+                double centerX = ex + eW / 2;
+                double centerY = ey + eH / 2;
+                new Explosion(_canvas, centerX, centerY);
+
+                enemy.Model.TakeDamage();
+
+                if (enemy.Model.Lives <= 0)
                 {
-                    if (wall.IsDestroyed)
-                        continue;
+                    int points = enemy.Model.Type == EnemyType.Heavy
+                        ? HeavyEnemyPoints
+                        : BasicEnemyPoints;
 
-                    double wx = Canvas.GetLeft(wall.Img);
-                    double wy = Canvas.GetTop(wall.Img);
-                    double ww = wall.Img.Width;
-                    double wh = wall.Img.Height;
-
-                    // перевірка коорд кулі та стіни
-                    bool isHitWall = bullX < wx + ww && bullX + bullwidth > wx
-                                   && bullY < wy + wh && bullY + bullhght > wy;
-
-                    if (!isHitWall)
-                        continue;
-
-                    bullet.Deactivate(); //куля потрапила і зникає
-
-                    double centerX = wx + ww / 2;
-                    double centerY = wy + wh / 2;
-                    new Explosion(_canvas, centerX, centerY);
-                    if (wall.Type == TypeWall.Brick) //  цегла
-                        wall.TakeHit();
-                    hitSmth = true;
-                    break;
-                }
-                if (hitSmth) continue;
-
-                if (bullet.OwnerEnemy != null)
-                {
-                    double px = Canvas.GetLeft(_playerImg);
-                    double py = Canvas.GetTop(_playerImg);
-                    double pW = _playerImg.Width;
-                    double pH = _playerImg.Height;
-
-                    bool hitPlayer = bullX < px + pW
-                                  && bullX + bullwidth > px
-                                  && bullY < py + pH
-                                  && bullY + bullhght > py;
-
-                    if (hitPlayer)
-                    {
-                        bullet.Deactivate();
-                        double centerX = px + pW / 2;
-                        double centerY = py + pH / 2;
-                        new Explosion(_canvas, centerX, centerY);
-
-                        _canvas.Children.Remove(bullet.Img);
-                        _bullets.Remove(bullet);
-
-                        _playerModel.TakeDamage();
-                        PlayerHit.Invoke();
-
-                        continue;
-                    }
+                    EnemyKilled?.Invoke(points);
                 }
 
-                if (bullet.OwnerEnemy == null)
-                    foreach (var enemy in _enemies)
-                    {
-                        if (enemy.Img.Visibility != Visibility.Visible)
-                            continue;
-
-                        if (bullet.OwnerEnemy == enemy.Model) //пропуск ітерації якщо куля власника
-                            continue;
-
-                        double ex = Canvas.GetLeft(enemy.Img);
-                        double ey = Canvas.GetTop(enemy.Img);
-                        double eW = enemy.Img.Width;
-                        double eH = enemy.Img.Height;
-
-                        bool hitEnemy = bullX < ex + eW
-                                         && bullX + bullwidth > ex
-                                         && bullY < ey + eH
-                                         && bullY + bullhght > ey;
-
-                        if (!hitEnemy)
-                            continue;
-
-
-                        bullet.Deactivate();//куля влучила в бота
-
-                        double centerX = ex + eW / 2;
-                        double centerY = ey + eH / 2;
-                        new Explosion(_canvas, centerX, centerY);
-
-                        enemy.Model.TakeDamage();
-                        if (enemy.Model.Lives <= 0)
-                        {
-                            int points = enemy.Model.Type == EnemyType.Heavy
-                                ? HeavyEnemyPoints
-                                : BasicEnemyPoints;
-                            enemy.Img.Visibility = Visibility.Collapsed; //приховуємо спрайт
-                            EnemyKilled.Invoke(100);
-                        }
-                        break;
-                    }
-
+                break;
             }
         }
     }
